c9ee8eae8e7991078ba32f47ee15b71ff6dd60d0
Jaklin Jeleva <jaklinj@abv.bg>
2024-02-03 16:47:22 +0200

    Render Home Page

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const path = require('path');
 const app = express();
 const port = 4000;
 
+
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('home', { layout: false });
