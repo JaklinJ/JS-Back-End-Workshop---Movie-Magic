7469fc2cb2a1c1c8bbc77ea0106537e93421840e
Jaklin Jeleva <jaklinj@abv.bg>
2024-02-04 15:34:34 +0200

    extract express and handlebars commits

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
 
 const routes = require('./routes')
+const configHandlebars = require('./config/configHandlebars');
+const configExpress = require('./config/cofigExpress')
 
 const app = express();
 const port = 4000;
 
+configHandlebars(app);
+configExpress(app);
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes);
 
