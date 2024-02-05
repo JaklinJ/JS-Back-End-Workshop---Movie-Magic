f12d79e92513f340cbcfc005599560bcff43dc0f
Jaklin Jeleva <jaklinj@abv.bg>
2024-02-05 14:29:39 +0200

    add movie ids and details link

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,13 +1,14 @@
 const movies = [
-    {
-        title: 'Pinocchio',
-        genre: 'Adventure',
-        director: ' Robert Zemeckis',
-        date: '2022',
-        imageUrl: 'https://musicart.xboxlive.com/7/0e875100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080',
-        rating: '5.1',
-        description: "Hopeful of changing his life with a miracle, sad-eyed Italian woodcarver Geppetto puts the finishing touches on his latest creation. And having put his heart into making the wooden Pinocchio marionette, the lonely artisan takes a step back and makes a wish on a bright star. But the universe always listens to a longing heart. As a result, the mysterious magic of the benevolent Blue Fairy takes over, granting Geppetto's wish in the dead of night. As the effervescent essence of life reanimates the newly built puppet, innocent Pinocchio has everything he needs to be a boy. Now he is the living proof that no request is too extreme. However, can smiling Pinocchio prove himself brave, truthful, and unselfish?"
-      }
+  {
+    _id: 1,
+    title: 'Pinocchio',
+    genre: 'Adventure',
+    director: ' Robert Zemeckis',
+    date: '2022',
+    imageUrl: 'https://musicart.xboxlive.com/7/0e875100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080',
+    rating: '5.1',
+    description: "Hopeful of changing his life with a miracle, sad-eyed Italian woodcarver Geppetto puts the finishing touches on his latest creation. And having put his heart into making the wooden Pinocchio marionette, the lonely artisan takes a step back and makes a wish on a bright star. But the universe always listens to a longing heart. As a result, the mysterious magic of the benevolent Blue Fairy takes over, granting Geppetto's wish in the dead of night. As the effervescent essence of life reanimates the newly built puppet, innocent Pinocchio has everything he needs to be a boy. Now he is the living proof that no request is too extreme. However, can smiling Pinocchio prove himself brave, truthful, and unselfish?"
+  }
 ];
 
 exports.getAll = () => {
@@ -16,5 +17,8 @@ exports.getAll = () => {
 
 
 exports.create = (movieData) => {
+
+  movieData._id = movies[movies.length-1]._id + 1;
+
    movies.push(movieData); 
 }
